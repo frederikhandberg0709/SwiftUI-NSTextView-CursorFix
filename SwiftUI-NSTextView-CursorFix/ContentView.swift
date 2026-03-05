1e78a5827544be21cea6e4665bb0b3b713343cc2
Frederik Handberg <hello@frederikhandberg.com>
2026-03-05 09:19:26 +0100

    Initial Commit

---
+++ SwiftUI-NSTextView-CursorFix/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI-NSTextView-CursorFix
+//
+//  Created by Frederik Handberg on 05/03/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI-NSTextView-CursorFix/SwiftUI_NSTextView_CursorFixApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_NSTextView_CursorFixApp.swift
+//  SwiftUI-NSTextView-CursorFix
+//
+//  Created by Frederik Handberg on 05/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_NSTextView_CursorFixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
